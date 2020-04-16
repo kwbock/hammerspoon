58469639a8897f1886a025aced07a4179974ec23
Kyle Bock <kyle.bock@salesloft.com>
2020-04-16 12:18:45 -0400

    updates

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ modal.setHotkey({"cmd", "ctrl"}, "space")
 
 hs.hints.style = "vimperator"
 hs.hints.showTitleThresh = 4
-hs.hints.titleMaxSize = 10
+hs.hints.titleMaxSize = 20
 hs.hints.fontSize = 30
 
 require("windows").addState(modal)
@@ -16,5 +16,16 @@ require("multimedia").addState(modal)
 
 local stateMachine = modal.createMachine()
 stateMachine:toMain()
+
+-- Use Cmd+\ to show 1Password everywhere except Visual Studio Code
+function activate_1password()
+  local client = hs.application.frontmostApplication()
+  if client:title() == 'Code' then
+    hs.eventtap.keyStroke({"cmd"}, "f11")
+  else
+    hs.eventtap.keyStroke({"cmd"}, "f12")
+  end
+end
+hs.hotkey.bind({"cmd"}, "\\", activate_1password)
 
 hs.alert.show("Config Loaded")
